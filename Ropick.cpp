dd45b812cfc8893ee2f78dfab3dbe3a93aafb4b0
ОЛЕСЯ АВИЛОВА <alesia.560limp@gmail.com>
2025-04-08 19:09:35 +0300

    отделение_цикла_1

--- Ropick.cpp
+++ Ropick.cpp
@@ -12,6 +12,7 @@ struct STUD {
 double calculateAverage(const double ses[], int size) {
     double sum = 0.0;
     for (int i = 0; i < size; ++i) {
+
         sum += ses[i];
     }
     return sum / size;
